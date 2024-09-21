e6fc243eb31377e109b2f53b73bdfb5c3e6d64ae
Becker Espinoza <becker.espinoza01rivera@gmail.com>
2024-09-21 13:38:16 -0600

    versionar tipos de datos - ejercicios Vimos nullables

---
+++ Ejercicios/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+
+      /*  string courseName = "Academia SITIC";
+        String courseName2 = "AcademiaSITIC";
+
+        int studentCount = 28;
+        bool isStartingNow = true;
+
+        int? age = null;
+        age = 23;
+
+        Console.WriteLine(age != null ? age : 0);
+        Console.ReadKey(); */
+
+    }
+
+    public class User
+    {
+        
+        //Forma corta
+        public int IdUser { get; set; }
+
+        //Forma larga
+        private string _name;
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string SetName(string name)
+        {
+            return name;
+        }
+    }
+
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
