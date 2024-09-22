8d5f802a0b3216496c987732fc64484e8cdaebf4
Becker Espinoza <becker.espinoza01rivera@gmail.com>
2024-09-22 15:06:39 -0600

    tema: metodos

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 internal class Program
 {
@@ -6,18 +7,32 @@ internal class Program
     {
         // Console.WriteLine("Hello, World!");
 
-      /*  string courseName = "Academia SITIC";
-        String courseName2 = "AcademiaSITIC";
+        /*  string courseName = "Academia SITIC";
+          String courseName2 = "AcademiaSITIC";
 
-        int studentCount = 28;
-        bool isStartingNow = true;
+          int studentCount = 28;
+          bool isStartingNow = true;
 
-        int? age = null;
-        age = 23;
+          int? age = null;
+          age = 23;
 
-        Console.WriteLine(age != null ? age : 0);
-        Console.ReadKey(); */
+          Console.WriteLine(age != null ? age : 0);
+          Console.ReadKey(); */
 
+        //------------------------------------------------
+
+        //METODOS
+        int a = 5;
+        int b = 10;
+
+        Console.WriteLine(SumInteger(a, b));
+        Console.ReadKey();
+    }
+
+    public static int SumInteger(int a, int b)
+    {
+        a = b;
+        return a + b;
     }
 
     public class User
